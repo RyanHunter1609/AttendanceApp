e5f7b3d5e1210db38ab02d5cd98c4cda414f13c7
Ryan Hunter <rhunter4@xula.edu>
2018-01-22 10:21:27 -0600

    new attendence app forked from AEDWARDS

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\n\nHello, AttendanceApp!\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine().toUpperCase();
+        System.out.println("Welcome " + name + "!");
     }
 }
