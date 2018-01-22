a6062b52ef1ec31dbe9a2bf181ab2e30384fe6c1
Ryan Hunter <rhunter4@xula.edu>
2018-01-22 10:36:53 -0600

    calculate the number of students with perfect attendance

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,16 @@ public class Main {
             absences.add(random.nextInt(11));
         }
         System.out.println("The elements are: " + absences);
+
+        //calculate the number of students with perfect attendance
+        int countPerfect = 0;
+        for (int i = 0; i < name.length(); i++) {
+            if (absences.get(i) == 0) {
+                countPerfect++;
+            }
+        }
+        System.out.println("The number of students with perfect attendance: " + countPerfect);
+
+
     }
 }
